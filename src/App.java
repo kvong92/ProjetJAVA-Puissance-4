b4603f9122a4c6cb82ded4c3797af8a4fadc5cc8
Kevin VONG <vongkevin.c92@gmail.com>
2023-02-01 15:17:25 +0100

    addPlayer

--- src/App.java
+++ src/App.java
@@ -1,6 +1,4 @@
 package src;
-
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import model.Player;
@@ -47,82 +45,113 @@ public class App {
         menus.add("6 - PURPLE");
         menus.add("7 - CYAN");
         menus.add("8 - WHITE");
-        menus.add("q - Quitter");
         menus.add("Veuillez entrer un choix");
         for (String menu : menus) {
             System.out.println(menu);
         }
     }
 
-    public static Player createPlayer() throws ParseException {
+    public static Player createPlayer() {
         Player newPlayer = new Player();
-        System.out.println("Entrez votre nom : ");
-        String name = scan.nextLine();
-        newPlayer.setName(name);
-        System.out.println("Entrez votre symbole : ");
-        String symbol = scan.nextLine();
-        newPlayer.setSymbol(symbol);
+        while (true) {
+            System.out.println("Entrez votre nom : ");
+            try {
+                String name = scan.nextLine();
+                newPlayer.setName(name);
+                break;
+            } catch (Exception e) {
+                System.out.println("Veuillez entrer un nom valide (non vide)");
+            }
+        }
+        while (true) {
+            System.out.println("Entrez votre symbole : ");
+            try {
+                String symbol = scan.nextLine();
+                newPlayer.setSymbol(symbol);
+                break;
+            } catch (Exception e) {
+                System.out.println("Veuillez entrer un symbole valide (1 caractère et non vide)");
+            }
+        }
         while (true) {
             System.out.println("Choississez votre couleur : ");
             showColorMenu();
+
             String choice = scan.nextLine();
             switch (choice) {
                 case "1" -> {
                     newPlayer.setSymbolColor(BLACK);
+                    break;
                 }
                 case "2" -> {
                     newPlayer.setSymbolColor(RED);
+                    break;
                 }
                 case "3" -> {
                     newPlayer.setSymbolColor(GREEN);
+                    break;
                 }
                 case "4" -> {
                     newPlayer.setSymbolColor(YELLOW);
+                    break;
                 }
                 case "5" -> {
                     newPlayer.setSymbolColor(BLUE);
+                    break;
                 }
                 case "6" -> {
                     newPlayer.setSymbolColor(PURPLE);
+                    break;
                 }
                 case "7" -> {
                     newPlayer.setSymbolColor(CYAN);
+                    break;
                 }
                 case "8" -> {
                     newPlayer.setSymbolColor(WHITE);
+                    break;
                 }
-                default -> System.out.println("Veuillez choisir une option valable");
+                default -> {
+                    System.out.println("Veuillez entrer un nombre entre 1 et 8, correspondant aux couleurs disponibles");
+                    continue;
+                }
             }
-            return newPlayer;
+            break;
         }
+        return newPlayer;
     }
 
     public static void main(String[] args) throws Exception {
         while (true) {
-            showMenu();
-            String choice = scan.nextLine();
-            switch (choice) {
-                case "1" -> {
-                    System.out.println("Player vs Player PlayVsPlayer()");
-                    Player NouveauJoueur = createPlayer();
-                    System.out.println("Nom : " + NouveauJoueur.getNom());
-                    System.out.println("Symbole : " + NouveauJoueur.getSymbol());
-                    System.out.println("Couleur : ");
-                    System.out.println(NouveauJoueur.getSymbolColor() + "Couleur" + RESET);
-                    return;
+            try {
+                showMenu();
+                String choice = scan.nextLine();
+                switch (choice) {
+                    case "1" -> {
+                        System.out.println("Player vs Player PlayVsPlayer()");
+                        Player NouveauJoueur = createPlayer();
+                        System.out.println("Nom : " + NouveauJoueur.getNom());
+                        System.out.println("Symbole : " + NouveauJoueur.getSymbol());
+                        System.out.println("Couleur : ");
+                        System.out.println(NouveauJoueur.getSymbolColor() + "Couleur" + RESET);
+                        return;
+                    }
+                    case "2" -> {
+                        System.out.println("Jouer VS IA PlayVsIa()");
+                    }
+                    case "3" -> {
+                        System.out.println("Affichage du TOP 10 ShowTop10()");
+                    }
+                    case "q" -> {
+                        System.out.println("Fermeture de l'application ...");
+                        return;
+                    }
+                    default -> System.out.println("Veuillez choisir une option valable");
                 }
-                case "2" -> {
-                    System.out.println("Jouer VS IA PlayVsIa()");
-                }
-                case "3" -> {
-                    System.out.println("Affichage du TOP 10 ShowTop10()");
-                }
-                case "q" -> {
-                    System.out.println("Fermeture de l'application ...");
-                    return;
-                }
-                default -> System.out.println("Veuillez choisir une option valable");
+            } catch (Exception e) {
+                System.out.println("Veuillez choisir une option valable");
             }
+
         }
     }
 }
